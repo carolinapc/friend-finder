263326c69ebd92e374022fa306acf2b2adc139f7
Carolina Cavalcanti <carolinapc@gmail.com>
2019-08-18 14:35:24 -0400

    implemented front-end validation

--- app/public/assets/javascript/survey.js
+++ app/public/assets/javascript/survey.js
@@ -14,36 +14,61 @@ const questions = [
 
 $(document).ready(() => {
     var $questions = $("#questions");
+
+    //remove the invalidfield class for all the elements that are siblings of the clicked label from the radio button
+    var removeClass = function (){
+        $(this).siblings().removeClass("invalidfield");
+    }
     
+    //creates the questions and its radio buttons elements
     questions.forEach((item, index) => {
         let $newHeader = $("<h5>").text(item);
         $questions.append($newHeader);
 
-        for (let i = 1; i <= 5; i++){
-            let $newDiv = $(`<div class="form-check form-check-inline">`);
-            let $newRadio = $(`<input class="form-check-input question${index}" type="radio" name="question${index}" value="${i}">`);
-            let $newLabel = $(`<label class="form-check-label" for="question${index}"> ${i} </label>`);
+        let $newDiv = $(`<div class="btn-group btn-group-toggle" data-toggle="buttons">`);
 
-            $newDiv.append($newRadio);
+        for (let i = 1; i <= 5; i++) {
+            let tip = "";
+            if (i === 1) tip = "(strongly disagree)";
+            if (i === 5) tip = "(strongly agree)";
+
+            let $newLabel = $(`<label class="btn btn-outline-primary">${i} ${tip}</label>`);
+            $newLabel.click(removeClass);
+            let $newRadio = $(`<input type="radio" required name="question${index}" value="${i}" autocomplete="off">`);
+
+            $newLabel.append($newRadio);
             $newDiv.append($newLabel);
-            $questions.append($newDiv);
         }
-
+        $questions.append($newDiv);
     });
-
+    
+    
     $("#submit").click(() => {
         event.preventDefault();
-        var person = {};
 
-        person.name = $("#name").val();
-        person.photo = $("#photo").val();
+        //remove the invalidfield class from the elements that was fixed by the user after the first submission
+        $("input[type=radio]").parents('label').removeClass("invalidfield");
+        
+        //checks whether or not are invalid fields
+        if(form.checkValidity() === false){
+            form.classList.add('was-validated');
+            event.preventDefault();
+            event.stopPropagation();
+            $("input[type=radio]:invalid").parents('label').addClass("invalidfield");
+
+            return false;
+        }      
+        
+        var person = {};
+        
+        person.name = $("#name").val().trim();
+        person.photo = $("#photo").val().trim();
 
         person.scores = [];
         for (let i = 0; i < questions.length; i++){
             person.scores.push($(`input[name='question${i}']:checked`).val());
         }
-
-        console.log(person);
+        
         $.post("/api/friends", person, data => {
             
             console.log(data); 
