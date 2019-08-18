e333abfc1fa21e7d60f2323df741c704c34f5fee
Carolina Cavalcanti <carolinapc@gmail.com>
2019-08-18 11:04:02 -0400

    implemented server side validation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,25 +12,51 @@ module.exports = app => {
         var newFriend = req.body;
         var friendMatched;
         
-        var lessDifference = 100; //sets an initial value higher than the greater possible number
-        for(person of friends) {
-            var totalDifference = 0;
+        try {
 
-            //sum all the differences on the score
-            for (var i = 0; i < person.scores.length; i++){
-                totalDifference += Math.abs(parseInt(person.scores[i]) - parseInt(newFriend.scores[i])); 
+            //----fields validation
+            if (newFriend.name.trim() === "") {
+                throw new Error("Name is required");
             }
+            if (newFriend.photo.trim() === "") {
+                throw new Error("Link to the photo image is required");
+            }
+            if (newFriend.scores.length !== 10) {
+                throw new Error("You must send all the 10 questions score");
+            }
+
+            newFriend.scores.map(x => {
+                if (x === "") throw new Error("All the scores must be filled");
+                if (isNaN(x)) throw new Error("All the scores must be numbers");
+                if (x > 5 || x < 1) throw new Error("All the scores must be from 1 to 5");
+                if (!Number.isInteger(Number(x))) throw new Error("All the scores must be integers");
+            });
+            //----
+
+            var lessDifference = 100; //sets an initial value higher than the greater possible number
+            for (var x = 0; x < friends.length; x++){
+                var person = friends[x];
+                var totalDifference = 0;
+
+                //sum all the differences on the score
+                for (var i = 0; i < person.scores.length; i++){
+                    totalDifference += Math.abs(Number(person.scores[i]) - Number(newFriend.scores[i])); 
+                }
+                
+                //check if the current total difference is less than less one
+                if (totalDifference < lessDifference) {
+                    lessDifference = totalDifference;
+                    friendMatched = person;
+                }
+
+            };
+
+            friends.push(newFriend); //add newFriend into the Friends array
+            res.json({success:true, match: friendMatched});
+        } catch (err) {
             
-            //check if the current total difference is less than less one
-            if (totalDifference < lessDifference) {
-                lessDifference = totalDifference;
-                friendMatched = person;
-            }
-
-        };
-
-        friends.push(newFriend);
-        res.json(friendMatched);
-        
+            res.json({success:false, message: err.message})
+        }
+    
     });
 }
